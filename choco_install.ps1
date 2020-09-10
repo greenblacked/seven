e165627e81e337b1ceba8afacbbfb24f67efc1cf
Serhii <zolotov.98@gmail.com>
2020-09-10 15:58:56 +0300

    Update choco_install.ps1

--- choco_install.ps1
+++ choco_install.ps1
@@ -1,10 +1,3 @@
-### install chocolatey ###
-#if(-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall")){
-    #iex ((new-object net.webclient).DownloadString("https://chocolatey.org/install.ps1"))
-#}
- #Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 cinst pester -version 2.0.2
 
